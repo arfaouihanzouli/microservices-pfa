0d21c418d42e0464cb8703d2d0e13614fd6fe251
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-09 21:57:49 +0100

    add eureka client to microservices

--- microservice-users/src/main/java/com/pfa/microserviceusers/MicroserviceUsersApplication.java
+++ microservice-users/src/main/java/com/pfa/microserviceusers/MicroserviceUsersApplication.java
@@ -2,8 +2,10 @@ package com.pfa.microserviceusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceUsersApplication {
 
 	public static void main(String[] args) {

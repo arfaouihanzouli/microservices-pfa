e8288f03efe318a33f3b44fc8f77d45ddccbb3fc
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-03-31 02:16:43 +0100

    add responses of offers

---
+++ microservice-offers/src/main/java/com/pfa/microserviceoffers/responses/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.pfa.microserviceoffers.responses;
+
+import java.io.Serializable;
+
+public class ApiResponse implements Serializable {
+
+    private String message;
+    private boolean status;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(String message, boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}

a0219532db0404f028c10a1253976f3b32112770
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-04-01 21:43:36 +0100

    complete candidat bean

--- microservice-cv/src/main/java/com/pfa/microservicecv/beans/CandidatBean.java
+++ microservice-cv/src/main/java/com/pfa/microservicecv/beans/CandidatBean.java
@@ -1,4 +1,23 @@
 package com.pfa.microservicecv.beans;
 
-public class CandidatBean {
+import java.io.Serializable;
+
+public class CandidatBean implements Serializable {
+
+    private Long id;
+
+    public CandidatBean(Long id) {
+        this.id = id;
+    }
+
+    public CandidatBean() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
